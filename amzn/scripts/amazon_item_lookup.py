d712dc84647c2f3bc9bab766ef01982a633e556a
Anthony Young <ayoung87@gmail.com>
2017-02-11 16:56:54 -0800

    Require id_type arg

--- amzn/scripts/amazon_item_lookup.py
+++ amzn/scripts/amazon_item_lookup.py
@@ -8,10 +8,9 @@ from ..common import VALID_ID_TYPES
 def main():
     description = "Look up items in Amazon's movie library."
     argparser = argparse.ArgumentParser(description=description)
-    argparser.add_argument('id_type', nargs='?', default='UPC', choices=VALID_ID_TYPES,
-                           help='ID type of the item(s) to look up (default: UPC)')
-    argparser.add_argument('item_id', nargs='+',
-                           help='item ID to look up')
+    argparser.add_argument('id_type', choices=VALID_ID_TYPES,
+                           help='ID type of the item(s) to look up')
+    argparser.add_argument('item_id', nargs='+', help='item ID to look up')
     args = argparser.parse_args()
     api = API()
     result = api.item_lookup(item_id=args.item_id, id_type=args.id_type)
